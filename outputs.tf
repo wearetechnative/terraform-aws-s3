2df300694acecd8ca62330a039b4a25eae20b07d
Pim Snel <post@pimsnel.com>
2024-10-28 11:31:26 +0100

    add s3 id in outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,9 @@ output "s3_arn" {
   value = aws_s3_bucket.this.arn
 }
 
+output "s3_id" {
+  value = aws_s3_bucket.this.id
+}
 output "s3_name_parameter_arn" {
   value = aws_ssm_parameter.this.arn
 }
